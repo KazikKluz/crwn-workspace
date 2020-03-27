047f453797214db201adf8f7941b424ee3e69bc0
KazikKluz <kazik.kluz@gmail.com>
2020-03-27 16:53:13 +0000

    71 done

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import CollectionItem from "../collection-item/collection-item.component.jsx";
+
+import "./collection-preview.scss";
 
 const CollectionPreview = ({ title, items }) => {
   return <div className="collection-preview">
            <h1 className="title">{title.toUpperCase()}</h1>
            <div className="preview">
              {items.filter((item, idx)=> idx < 4 ) 
-              .map((item)=>{
-               return <div key={item.id}>
-                        {item.name}
-                      </div>;
+              .map(({id, ...otherCollectionProps})=>{
+                return  <CollectionItem key={id} {...otherCollectionProps} />;
              })}
            </div>
          </div>;
