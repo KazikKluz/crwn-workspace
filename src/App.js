830e2ea3602c52d5372e24f4cf98e73d573e6436
KazikKluz <kazik.kluz@gmail.com>
2020-04-01 20:30:33 +0100

    72 done

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
 
 import './App.css';
 import './homepage.styles.scss';
 
 import HomePage from './homepage.component';
 
-const HatsPage = () => {
-  return <div>
-           <h1>Hats Page</h1>
-         </div>;
-};
-
 function App() {
   return (
-    <div> 
+    <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route exact path="/shop" component={ShopPage}/>
