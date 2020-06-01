1539e54937ea4051701d8ecbf2499eb51d88e0a5
KazikKluz <kazik.kluz@gmail.com>
2020-06-01 18:44:07 +0100

    113 done

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -4,15 +4,18 @@ import CollectionItem from "../collection-item/collection-item.component.jsx";
 import "./collection-preview.scss";
 
 const CollectionPreview = ({ title, items }) => {
-  return <div className="collection-preview">
-           <h1 className="title">{title.toUpperCase()}</h1>
-           <div className="preview">
-             {items.filter((item, idx)=> idx < 4 ) 
-              .map(({id, ...otherCollectionProps})=>{
-                return  <CollectionItem key={id} {...otherCollectionProps} />;
-             })}
-           </div>
-         </div>;
-}
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => {
+            return <CollectionItem key={item.id} item={item} />;
+          })}
+      </div>
+    </div>
+  );
+};
 
 export default CollectionPreview;
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,4 +4,9 @@ const toggleCartHidden = () => {
   return { type: cartActionTypes.TOGGLE_CART_HIDDEN };
 };
 
+export const addItem = (item) => ({
+  type: cartActionTypes.ADD_ITEM,
+  payload: item,
+});
+
 export default toggleCartHidden;
