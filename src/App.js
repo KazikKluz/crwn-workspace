68b04d476d3be3ec425e0ca7f395b87cafcc6637
KazikKluz <kazik.kluz@gmail.com>
2020-06-07 19:49:18 +0100

    memoizing state selection by adding selectors to all mapStateToProps functions 121

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
 import { setCurrentUser } from "./redux/user/user.actions.js";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx";
+import { selectCurrentUser } from "./redux/user/user.selector";
 
 import "./App.css";
 import "./homepage.styles.scss";
@@ -66,9 +69,9 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => {
-  return { currentUser: user.currentUser };
-};
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
 
 const mapDispatchToProps = (dispatch) => {
   return { setCurrentUser: (user) => dispatch(setCurrentUser(user)) };
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+
 import { selectCartItems } from "../../redux/cart/cart.selector";
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
+import { createStructuredSelector } from "reselect";
 
 const Cart = ({ cartItems }) => (
   <div className="cart-dropdown">
@@ -18,8 +20,8 @@ const Cart = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  cartItems: selectCartItems(state),
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
 });
 
 export default connect(mapStateToProps)(Cart);
