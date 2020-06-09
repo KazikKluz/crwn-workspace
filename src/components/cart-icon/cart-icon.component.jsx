df0ab1ff8a2e7fc0913ade9bbdb42390daa7936e
KazikKluz <kazik.kluz@gmail.com>
2020-06-09 16:30:40 +0100

    added reselect library and added it to cart selectors to memoize them

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
+
 import toggleCartHidden from "../../redux/cart/cart.actions.js";
 
 import "./cart-icon.styles.scss";
@@ -14,11 +16,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
-    0
-  ),
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
