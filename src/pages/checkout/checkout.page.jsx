397ffa8d30cc8adec224b5a70b8612d821ca56cc
KazikKluz <kazik.kluz@gmail.com>
2020-06-09 18:59:13 +0100

    finished checkout page, added new selector for calculating the total price 123

--- src/pages/checkout/checkout.page.jsx
+++ src/pages/checkout/checkout.page.jsx
@@ -1,5 +1,43 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-const Checkout = () => <div>Checkout</div>;
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selector";
 
-export default Checkout;
+import "./checkout.styles.scss";
+
+const Checkout = ({ cartItems, total }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-blocks">
+        <span>Description</span>
+      </div>
+      <div className="header-blocks">
+        <span>Quantity</span>
+      </div>
+      <div className="header-blocks">
+        <span>Price</span>
+      </div>
+      <div className="header-blocks">
+        <span>Remove</span>
+      </div>
+      {cartItems.map((cartItem) => cartItem.name)}
+      <div className="total">
+        <span>TOTAL: ${total}</span>
+      </div>
+    </div>
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
+
+export default connect(mapStateToProps)(Checkout);
