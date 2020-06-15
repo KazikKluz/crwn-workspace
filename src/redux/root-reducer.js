a749a93b82da3e49b9fa8d460e9fc1d0cf0e07cb
KazikKluz <kazik.kluz@gmail.com>
2020-06-15 21:45:02 +0100

    implemented redux persist. now store.cart state is persisted in the localStorage, so app saves items in our cart for later 130

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,5 +1,12 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import userReducer from "./user/user.reducer.js";
 import cartReducer from "./cart/cart.reducer.js";
 
-export default combineReducers({ user: userReducer, cart: cartReducer });
+const persistConfig = { key: "root", storage, whitelits: ["cart"] };
+
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+
+export default persistReducer(persistConfig, rootReducer);
