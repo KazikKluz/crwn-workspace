bdaff7db3037b095bb10d298eb36fb8a543d29b3
KazikKluz <kazik.kluz@gmail.com>
2020-06-18 21:23:40 +0100

    imroved styling for collection item, directory data is now normalized so we search collection by key not with .find method over an array so performance improved, shop selector updated accordingly 138

--- src/pages/collection/collection-page.jsx
+++ src/pages/collection/collection-page.jsx
@@ -5,11 +5,16 @@ import { selectCollection } from "../../redux/shop/shop.selector";
 
 import "./collection-page.styles.scss";
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionId);
+const CollectionPage = ({ match, collection }) => {
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>Collection Page</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
