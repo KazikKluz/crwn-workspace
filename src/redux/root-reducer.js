ff8113bd2b9347acc386a0de7e25c0f01addea35
KazikKluz <kazik.kluz@gmail.com>
2020-06-19 15:53:33 +0100

    moved collection state into redux, new selector and reducer; shop component changed into functional component 132

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer.js";
 import cartReducer from "./cart/cart.reducer.js";
 import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 const persistConfig = { key: "root", storage, whitelits: ["cart"] };
 
@@ -12,6 +13,7 @@ const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
