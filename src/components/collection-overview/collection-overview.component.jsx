d26ece3eceec6efb0e7e39bef79b857ab373fc60
KazikKluz <kazik.kluz@gmail.com>
2020-06-20 22:36:43 +0100

    after normalization selectCollection doesnt work as it expect am array not an object, solution new selector added selectCollectionForPreview 140

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { selectCollections } from "../../redux/shop/shop.selector";
+import { selectCollectionForPreview } from "../../redux/shop/shop.selector";
 import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../../components/collection-preview/collection-preview";
 
@@ -14,7 +14,7 @@ const CollectionOverview = ({ collections }) => (
   </div>
 );
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
