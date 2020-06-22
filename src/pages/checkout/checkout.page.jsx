e5f5479ea07654cc0cb955bd7b4260f8855ef159
KazikKluz <kazik.kluz@gmail.com>
2020-06-22 09:45:40 +0100

    integrated stripe checkout button with the test credit card details 143

--- src/pages/checkout/checkout.page.jsx
+++ src/pages/checkout/checkout.page.jsx
@@ -7,6 +7,7 @@ import {
 } from "../../redux/cart/cart.selector";
 
 import CheckoutItem from "../../components/checkout-component/checkout.component";
+import StripeCheckout from "../../components/stripe-button/stripe-button.component";
 
 import "./checkout.styles.scss";
 
@@ -32,9 +33,13 @@ const Checkout = ({ cartItems, total }) => (
     {cartItems.map((cartItem) => {
       return <CheckoutItem cartItem={cartItem} key={cartItem.id} />;
     })}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
+    <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      Please use the following test credit card for payment
+      <br /> 4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
     </div>
+
+    <StripeCheckout price={total} />
   </div>
 );
 
