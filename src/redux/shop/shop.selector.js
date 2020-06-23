9512845e02dd1b6d938fa2b1d22ee9f7702d0b28
KazikKluz <kazik.kluz@gmail.com>
2020-06-23 15:56:01 +0100

    improved styling of collection item and page; shop data normalization: array into object, so performance improvement and updated selector 138

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,11 +1,4 @@
 import { createSelector } from "reselect";
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 2,
-  womens: 4,
-  mens: 5,
-};
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -14,8 +7,7 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   );
