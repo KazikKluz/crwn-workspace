ecf11c3ffa63359e14737633b646b89f94846244
KazikKluz <kazik.kluz@gmail.com>
2020-06-24 15:48:10 +0100

    added styled-components library and updated header and homepage using it 154

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 import Directory from "./components/directory-component/directory.component.jsx";
+import { HomePageContainer } from "./homepage.styles.js";
 
 const HomePage = () => (
-
-  <div className='homepage'>
-      <Directory />
-    </div>
-
+  <HomePageContainer className="homepage">
+    <Directory />
+  </HomePageContainer>
 );
 
 export default HomePage;
