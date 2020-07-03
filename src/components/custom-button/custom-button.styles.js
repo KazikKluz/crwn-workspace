303a692f0d40583219aab453f342351259d2f3c3
KazikKluz <kazik.kluz@gmail.com>
2020-07-03 19:52:12 +0100

    now data is pulled from firebase to shop component; new util function 166

--- src/components/custom-button/custom-button.styles.js
+++ src/components/custom-button/custom-button.styles.js
@@ -1,28 +1,37 @@
 import styled, { css } from "styled-components";
 
+const buttonStyles = css`
+  background-color: black;
+  color: white;
+  border: none;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+  }
+`;
+
 const invertedButtonStyles = css`
   background-color: white;
   color: black;
   border: 1px solid black;
+
+  &:hover {
+    background-color: black;
+    color: white;
+    border: none;
+  }
 `;
 
 const googleSignInStyles = css`
   background-color: #4285f4;
   color: white;
+  border: 1px solid #4285f4;
+
   &:hover {
     background-color: #357ae8;
     border: none;
-  }
-`;
-
-const buttonStyles = css`
-  background-color: black;
-  color: white;
-  border: none;
-  &:hover {
-    background-color: white;
-    color: black;
-    border: 1px solid black;
   }
 `;
 
@@ -42,14 +51,11 @@ export const CustomButtonContainer = styled.button`
   line-height: 50px;
   padding: 0 35px 0 35px;
   font-size: 15px;
-
   text-transform: uppercase;
   font-family: "Open Sans Condensed";
   font-weight: bolder;
-  border: none;
   cursor: pointer;
   display: flex;
   justify-content: center;
-
   ${getButtonStyles}
 `;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -58,6 +58,19 @@ export const addCollectionAndDocuments = async (
   await batch.commit();
 };
 
+export const convertCollectionSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+  console.log(transformedCollection);
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
