89583330c54e993ac8e83070319dfbcc0f114ea3
KazikKluz <kazik.kluz@gmail.com>
2020-07-04 21:28:59 +0100

    added react redux, redux and logger modules, redux is partially set up; root and user reducers 101

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './App';
+import { Provider } from "react-redux";
+
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter>
-  <App />
+    <Provider>
+      <App />
+    </Provider>
   </BrowserRouter>,
-  document.getElementById('root'));
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
