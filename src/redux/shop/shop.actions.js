2eb9bad0a70729710296c9c41a47e1720ff97513
KazikKluz <kazik.kluz@gmail.com>
2020-07-08 21:51:38 +0100

    added redux thunk, async api call for getting the collection data is now managed by redux 175

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,32 @@
 import shopActionTypes from "./shop.types";
+import {
+  firestore,
+  convertCollectionSnapshotToMap,
+} from "../../firebase/firebase.utils";
 
-export const updateCollections = (collectionsMap) => ({
-  type: shopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = () => ({
+  type: shopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart);
+    this.unsubscribeFromSnapshot = collectionRef
+      .onSnapshot(async (snapShot) => {
+        const collectionsMap = convertCollectionSnapshotToMap(snapShot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: shopActionTypes.FETCH_COLLECTION_SUCCESS,
   payload: collectionsMap,
+});
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: shopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
 });
