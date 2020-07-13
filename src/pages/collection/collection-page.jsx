cc2b7b17332bf3199c045bc13a772cb46fa7f683
KazikKluz <kazik.kluz@gmail.com>
2020-07-13 16:07:55 +0100

    collections have dynamic routing; new shop selector for that 136

--- src/pages/collection/collection-page.jsx
+++ src/pages/collection/collection-page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
+
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import "./collection-page.styles.scss";
+
+import { selectCollection } from "../../redux/shop/shop.selector";
 
 const CollectionPage = ({ match }) => {
   console.log(match.path.collectionId);
@@ -12,4 +16,8 @@ const CollectionPage = ({ match }) => {
   );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
