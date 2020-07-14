3efad990891cb87650e378350f5752419137a701
KazikKluz <kazik.kluz@gmail.com>
2020-07-14 22:14:32 +0100

    added redux-saga library; created simle test generator functions 184

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
-import ReduxThunk from "redux-thunk";
-
+import createSagaMiddleware from "redux-saga";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer.js";
+import { fetchCollectionsStart } from "../redux/shop/shop.sagas";
 
-const middlewares = [ReduxThunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
