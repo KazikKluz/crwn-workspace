a72630a40b7a2cd26f3605cc7d06936e42335278
KazikKluz <kazik.kluz@gmail.com>
2020-07-22 22:12:10 +0100

    added checkout items with remove button to checkout page 126

--- src/pages/checkout/checkout.page.jsx
+++ src/pages/checkout/checkout.page.jsx
@@ -7,6 +7,8 @@ import {
   selectCartTotal,
 } from "../../redux/cart/cart.selector";
 
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 import "./checkout.styles.scss";
 
 const Checkout = ({ cartItems, total }) => (
@@ -15,22 +17,24 @@ const Checkout = ({ cartItems, total }) => (
       <div className="header-block">
         <span>Product</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Description</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Quantity</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Price</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Remove</span>
       </div>
-      {cartItems.map((cartItem) => cartItem.name)}
-      <div className="total">
-        <span>TOTAL: ${total}</span>
-      </div>
+    </div>
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
+    <div className="total">
+      <span>TOTAL: ${total}</span>
     </div>
   </div>
 );
