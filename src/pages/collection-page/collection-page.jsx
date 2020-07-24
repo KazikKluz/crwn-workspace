1d20e81398f319aacc420c00cbfcd5c80af238e9
KazikKluz <kazik.kluz@gmail.com>
2020-07-24 15:07:51 +0100

    improved naming of component; categorypage to collection page 135

---
+++ src/pages/collection-page/collection-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+const CollectionPage = (props) => {
+  console.log(props);
+  return (
+    <div className="collection">
+      <h2>Collection PAGE</h2>
+    </div>
+  );
+};
+
+export default CollectionPage;
