dbcb8878e9b49758d1f39e71ddafbb626ce3a33b
KazikKluz <kazik.kluz@gmail.com>
2020-07-29 13:28:00 +0100

    added sign up component to signinandsignuppage 93

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/sign-in.component.jsx";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 import "./sign-in-and-sign-up.scss";
 
 const SignInAndSignUpPage = () => {
-  return <div className="signInAndSignUp">
-           <SignIn />
-         </div>;
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+      <SignUp />
+    </div>
+  );
 };
 
 export default SignInAndSignUpPage;
