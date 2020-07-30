4b2412ca77362381fad97ae577f6b6fdc9568363
KazikKluz <kazik.kluz@gmail.com>
2020-07-30 09:46:08 +0100

    added user selectors and used createStructuredSelectors where possible 121

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
 import { connect } from "react-redux";
+
 import { setCurrentUser } from "./redux/user/user.actions.js";
+import { selectCurrentUser } from "./redux/user/user.selector";
+
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx";
@@ -11,6 +14,7 @@ import "./App.css";
 import "./homepage.styles.scss";
 
 import HomePage from "./homepage.component";
+import { createStructuredSelector } from "reselect";
 
 class App extends React.Component {
   constructor() {
@@ -66,9 +70,9 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => {
-  return { currentUser: user.currentUser };
-};
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
 
 const mapDispatchToProps = (dispatch) => {
   return { setCurrentUser: (user) => dispatch(setCurrentUser(user)) };
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,6 +7,7 @@ import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selector";
 
 import "./cart-dropdown.styles.scss";
+import { createStructuredSelector } from "reselect";
 
 const Cart = ({ cartItems }) => (
   <div className="cart-dropdown">
@@ -18,7 +19,7 @@ const Cart = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  cartItems: selectCartItems(state),
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
 });
 export default connect(mapStateToProps)(Cart);
