5b9af06c8530569a0841f96dae205a179d10d5c8
KazikKluz <kazik.kluz@gmail.com>
2020-07-31 21:37:05 +0100

    added nested dynamic routing in shop page 134

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CategoryPage from "../../pages/category-page/category-page";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
     </div>
   );
 };
