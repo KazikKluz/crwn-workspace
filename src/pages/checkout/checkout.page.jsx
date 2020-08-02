c35bff81693b36fe81fd711204709749d8da1d9a
KazikKluz <kazik.kluz@gmail.com>
2020-08-02 15:57:21 +0100

    add remove item from checkout functionality 128

--- src/pages/checkout/checkout.page.jsx
+++ src/pages/checkout/checkout.page.jsx
@@ -21,7 +21,7 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Description</span>
       </div>
       <div className="header-block">
-        <span>Quality</span>
+        <span>Quantity</span>
       </div>
       <div className="header-block">
         <span>Price</span>
