8f6e0a988e84030293fbf9bdafe46cea7dc937b7
KazikKluz <kazik.kluz@gmail.com>
2020-08-04 23:11:19 +0100

    crated backend payment route 218

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
-const { error } = require("console");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,6 +23,21 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  };
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
 
 app.listen(port, (error) => {
   if (error) throw error;
