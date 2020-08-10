ab8214acc06e1d9f3fbe25d2d7c27b753047d83f
KazikKluz <kazik.kluz@gmail.com>
2020-08-10 22:42:31 +0100

    created signinandsignup page and sign in component inside 75

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
+import SignInAndSignUpPage from "./pages/signInAndSignUp/signInAndSignUp";
 
-import './App.css';
-import './homepage.styles.scss';
+import "./App.css";
+import "./homepage.styles.scss";
 
-import HomePage from './homepage.component';
+import HomePage from "./homepage.component";
 
 function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route exact path="/shop" component={ShopPage}/>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
-      
     </div>
   );
 }
---
+++ src/pages/signInAndSignUp/signInAndSignUp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import "./signInAndSignUp.styles.scss";
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className="signInAndSignUp">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
