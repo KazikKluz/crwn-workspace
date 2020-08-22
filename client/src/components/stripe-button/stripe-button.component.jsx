dc2b01054a8dfdc1541873cd4213dfb8e0f0b7f5
KazikKluz <kazik.kluz@gmail.com>
2020-08-22 15:44:52 +0100

    connected client to server; test payments on 219

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeButton = ({ price }) => {
     "pk_test_51GvoSoAJujcbjWBpyDFXekJRSQKYSHZULfy5CZ4kfd3qbFH5FrSqy2S9XnLl4nNKQl6JtX077ykuWu0LVR4pSFqA00TNZwnCIe";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Succesful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Succesful");
+      })
+      .catch((error) => {
+        console.log("Payment error", JSON.parse(error));
+        alert(
+          "There was as issue with your payment. Please make suere you use the provided test credit card"
+        );
+      });
   };
 
   return (
