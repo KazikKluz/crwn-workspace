8c6e498f042ac756381f56aad6bbcd98d17c6bb0
KazikKluz <kazik.kluz@gmail.com>
2020-08-24 22:21:08 +0100

    added collection overview component inside shop to display collections 133

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,13 @@
 import React from "react";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.jsx";
-import SHOP_DATA from "../shop/shopData.js";
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
 
-class ShopPage extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-
-    const { collections } = this.state;
-    return <div className="shop-page">
-             {collections.map(({id, ...otherCollectionProps})=>{
-               return <CollectionPreview key={id} {...otherCollectionProps}/>;
-             })}
-           </div>;
-  }
-
-}
+const ShopPage = () => {
+  return (
+    <div className="shop-page">
+      <CollectionOverview />
+    </div>
+  );
+};
 
 export default ShopPage;
