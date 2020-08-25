36354e9afde7c8378bf74bf121d7aba220e47b61
KazikKluz <kazik.kluz@gmail.com>
2020-08-25 09:08:27 +0100

    now shopData is pulled from firestore 166

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import { Route } from "react-router-dom";
+
+import {
+  firestore,
+  convertCollectionSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
 
 import CollectionPage from "../../pages/collection/collection-page";
 
-const ShopPage = ({ match }) => {
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionOverview} />
-      <Route
-        exact
-        path={`${match.path}/:collectionId`}
-        component={CollectionPage}
-      />
-    </div>
-  );
-};
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshop = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+    this.unsubscribeFromSnapshop = collectionRef.onSnapshot(
+      async (snapshot) => {
+        convertCollectionSnapshotToMap(snapshot);
+      }
+    );
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          exact
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
