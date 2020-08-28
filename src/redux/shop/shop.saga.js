9a6206c527a998a6083bedb4ea8751be5a4a7867
KazikKluz <kazik.kluz@gmail.com>
2020-08-28 15:58:45 +0100

    moved async fetching into redux saga 185

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,9 +1,24 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, put } from "redux-saga/effects";
+import {
+  firestore,
+  convertCollectionSnapshotToMap,
+} from "../../firebase/firebase.utils";
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "../../redux/shop/shop.actions";
 
 import shopActionTypes from "./shop.types";
 
 export function* fetchCollectionAsync() {
-  yield console.log("I am fired");
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapShot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionSnapshotToMap, snapShot);
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionStart() {
