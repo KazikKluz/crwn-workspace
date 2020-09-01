2e4764e57485561c09aa1fa6d8bc4f837a4ebfd3
KazikKluz <kazik.kluz@gmail.com>
2020-09-01 15:37:43 +0100

    created a dynamic nested routes on shop 134

---
+++ src/components/category/category.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import "./category.styles.scss";
+
+const CategoryPage = () => <div className="category">Category Page</div>;
+
+export default CategoryPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CategoryPage from "../../components/category/category.component";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
     </div>
   );
 };
