b19ef624e9efc5ebb4ae3c2ae186ad71cf88ccdb
KazikKluz <kazik.kluz@gmail.com>
2020-09-07 17:14:31 +0100

    added Error Boundary component with falback page 256

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import { GlobalStyle } from "./global.styles";
 import "./homepage.styles.scss";
 
 import Checkout from "./pages/checkout/checkout.page";
+import ErrorBoundary from "./components/error-boundary/error-boundary.component";
 
 const HomePage = lazy(() => import("./homepage.component"));
 
@@ -26,18 +27,20 @@ const App = ({ currentUser, checkUserSession }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Suspense fallback={<div>... Loading</div>}>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route
-            exact
-            path="/signin"
-            render={() =>
-              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-            }
-          />
-        </Suspense>
+        <ErrorBoundary>
+          <Suspense fallback={<div>... Loading</div>}>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route
+              exact
+              path="/signin"
+              render={() =>
+                currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+              }
+            />
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
