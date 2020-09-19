597975bf193186e0da8fed0ddd31336d74a7ccc4
KazikKluz <kazik.kluz@gmail.com>
2020-09-19 20:02:50 +0100

    added more mobile responsiveness 250

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -20,30 +20,20 @@ import "./header.styles.scss";
 
 const Header = ({ currentUser, hidden, signOutStart }) => {
   return (
-    <HeaderContainer className="header">
-      <LogoContainer className="logo-container" to="/">
-        <Logo className="logo" />
+    <HeaderContainer>
+      <LogoContainer to="/">
+        <Logo />
       </LogoContainer>
-      <OptionsContainer className="options">
-        <OptionLink className="option" to="/shop">
-          SHOP
-        </OptionLink>
-        <OptionLink className="option" to="/shop">
-          CONTACT
-        </OptionLink>
+      <OptionsContainer>
+        <OptionLink to="/shop">SHOP</OptionLink>
+        <OptionLink to="/shop">CONTACT</OptionLink>
 
         {currentUser ? (
-          <OptionLink
-            as="div"
-            className="option"
-            onClick={() => signOutStart()}
-          >
+          <OptionLink as="div" onClick={() => signOutStart()}>
             SIGN OUT
           </OptionLink>
         ) : (
-          <OptionLink className="option" to="/signin">
-            SIGN IN
-          </OptionLink>
+          <OptionLink to="/signin">SIGN IN</OptionLink>
         )}
         <CartIcon />
       </OptionsContainer>
