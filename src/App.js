627bef170799e6e0528303441dce18ddf6ea6f4a
KazikKluz <kazik.kluz@gmail.com>
2020-09-24 18:34:36 +0100

    added shop page with collection preview 70

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch } from "react-router-dom";
-import './App.css';
-import './homepage.styles.scss';
+import "./App.css";
+import "./homepage.styles.scss";
 
-import HomePage from './homepage.component';
+import HomePage from "./homepage.component";
+import ShopPage from "./pages/shop/shop.page";
 
 const HatsPage = () => {
-  return <div>
-           <h1>Hats Page</h1>
-         </div>;
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
 };
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route exact path="/hats" component={HatsPage}/>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
-      
     </div>
   );
 }
