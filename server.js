0a27c045df2503b3c691fae38fd89eab6ace2165
KazikKluz <kazik.kluz@gmail.com>
2020-10-27 15:03:55 +0000

    backend payment route made 218

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -19,6 +21,22 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+
+  app.post("/payment", (req, res) => {
+    const body = {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    };
+
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).send({ error: stripeErr });
+      } else {
+        res.status(200).send({ success: stripeRes });
+      }
+    });
   });
 
   app.listen(port, (error) => {
