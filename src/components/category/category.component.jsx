61d2760fb444df8a002f5df18ff1432e38964810
KazikKluz <kazik.kluz@gmail.com>
2020-10-28 22:25:47 +0000

    improved confusing naming of collection page 135

--- src/components/category/category.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import CollectionItem from "../../components/collection-item/collection-item.component";
-import "./category.styles.scss";
-
-const CategoryPage = () => <div className="category">Category Page</div>;
-
-export default CategoryPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
-import CategoryPage from "../../components/category/category.component";
+import CollectionPage from "../../pages/collection/collection-page";
 
 const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionOverview} />
-      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+      <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
     </div>
   );
 };
